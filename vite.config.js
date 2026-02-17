ca6a1c50ce4dc10c65052fa8c4c35e268a84b2e6
WoodenKhrystyn-FS <kvwooden@student.fullsail.edu>
2026-02-17 14:05:36 -0500

    profile updates

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { execSync } from 'child_process';
 
-// Get the current Git branch name
-const gitBranch = execSync('git rev-parse --abrev-ref HEAD').toString().trim();
+// Get Git info at build time
+const gitCommitHash = execSync('git rev-parse --short HEAD').toString().trim();
+const gitBranch = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     __GIT_COMMIT_HASH__: JSON.stringify(gitCommitHash),
+    __GIT_BRANCH__: JSON.stringify(gitBranch),
   },
-})
+});
