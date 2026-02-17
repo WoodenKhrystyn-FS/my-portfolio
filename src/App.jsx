4d3120b2580913901c0f094d5933e4e117158d21
WoodenKhrystyn-FS <kvwooden@student.fullsail.edu>
2026-02-17 14:05:32 -0500

    Clean App.jsx and fix Git commit hash display

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import { useState } from "react";
 import kvlogo from "./assets/wooden.jpeg";
+
 import Navbar from "./Components/Navbar.jsx";
 import About from "./Components/About.jsx";
 import Hero from "./Components/Hero.jsx";
 import Contact from "./Components/Contact.jsx";
 import Projects from "./Components/Projects.jsx";
 import Skills from "./Components/Skills.jsx";
+
 import "./App.css";
 
 function App() {
   return (
     <>
-    <div>
-      <p>Current Version: {__GIT_COMMIT_HASH__}</p>
-    </div>
+      <div>
+        <p>Current Version: {__GIT_COMMIT_HASH__}</p>
+      </div>
       <div className="app">
         <Navbar />
         <About />
